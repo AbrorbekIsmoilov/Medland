ab560225c618edcdef187068bc751c9cff296221
Izzatillo0912 <izzatillo.umarov.808@gmail.com>
2023-05-30 08:20:52 +0300

    added new ui

--- app/src/main/java/com/med/medland/presentation/fragment/diagnosticFragments/DiagnosticsByTypeFragment.kt
+++ app/src/main/java/com/med/medland/presentation/fragment/diagnosticFragments/DiagnosticsByTypeFragment.kt
@@ -5,11 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.med.medland.databinding.FragmentDiagnosticBinding
+import com.med.medland.databinding.FragmentDiagnosticsByTypeBinding
 
-class DiagnosticFragment : Fragment() {
+class DiagnosticsByTypeFragment : Fragment() {
 
-    private lateinit var binding: FragmentDiagnosticBinding
+    private lateinit var binding : FragmentDiagnosticsByTypeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
